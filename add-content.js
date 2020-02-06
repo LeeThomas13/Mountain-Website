5a3510095ed4128ed0333c4e4d4ad0709356f793
LeeThomas13 <lmtthomasritz@gmail.com>
2020-02-05 16:52:46 -0800

    added multiple new things to java script functions

--- add-content.js
+++ add-content.js
@@ -4,15 +4,14 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Sup bro, wanna crush some white claws?'
+    greeting = 'Good dinnertime Bro! Wanna crush some white claws?'
 }   else if (hourNow > 12) {
-    greeting = 'Bro you eat lunch yet? No? Dope Lets hit that farmers market'
+    greeting = 'Good lunchtime Bro! You eat yet? No? Dope lets hit that farmers market'
 }   else if (hourNow > 0)  {
-    greeting = 'Bro I saw ur kegstand last night at kevins rager, sick moves'
+    greeting = 'Good morning Bro! I saw ur kegstand last night at kevin\'s rager, sick moves'
 } else {
     greeting = 'Aha bro welcome to the site'
 }
-
 document.write('<h3>' + greeting + '</h3>');
 
 
