3366b397772983c34fc3fb7ced912280e5278800
LeeThomas13 <lmtthomasritz@gmail.com>
2020-02-05 21:59:26 -0800

    added further javascript functionality

--- add-content-two.js
+++ add-content-two.js
@@ -3,12 +3,13 @@ var question = prompt('pick a number 1 or 2')
 var greeting;
 
 if (question == 1) {
-    greeting = prompt('Wow you really picked 1? why would you do that')
+    greeting = confirm('Wow you really picked 1? why would you do that')
 }   else if (question == 2) {
     greeting = confirm('dude you just won a snowboard!, sick! Email thisemailisfake@yahoo.com to claim it!')
 }   else if (question > 2)  {
-    greeting = prompt('you coulda won a free snowboard but you decided to not play by the rules >:(')
-}
+    greeting = confirm('you degenerate >:(')
+}    
+
 
 
 
