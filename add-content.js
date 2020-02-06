9a7a81298410025df8d8430387d3004078a7102b
LeeThomas13 <lmtthomasritz@gmail.com>
2020-02-05 16:55:46 -0800

    added minor text changes to java

--- add-content.js
+++ add-content.js
@@ -4,9 +4,9 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good dinnertime Bro! Wanna crush some white claws?'
+    greeting = 'Its dinnertime Bro! Wanna crush some white claws?'
 }   else if (hourNow > 12) {
-    greeting = 'Good lunchtime Bro! You eat yet? No? Dope lets hit that farmers market'
+    greeting =  'You eat lunch yet? No? Dope lets hit that farmers market'
 }   else if (hourNow > 0)  {
     greeting = 'Good morning Bro! I saw ur kegstand last night at kevin\'s rager, sick moves'
 } else {
