6a7ec29fe2cb014b6f9cfeb4aa22adbef1aede09
LeeThomas13 <lmtthomasritz@gmail.com>
2020-02-05 21:41:06 -0800

    updated java script outcomes

--- add-content-two.js
+++ add-content-two.js
@@ -5,7 +5,7 @@ var greeting;
 if (question == 1) {
     greeting = prompt('Wow you really picked 1? why would you do that')
 }   else if (question == 2) {
-    greeting = prompt('dude you just won a snowboard!, sick! Email thisemailisfake@yahoo.com to claim it!')
+    greeting = confirm('dude you just won a snowboard!, sick! Email thisemailisfake@yahoo.com to claim it!')
 }   else if (question > 2)  {
     greeting = prompt('you coulda won a free snowboard but you decided to not play by the rules >:(')
 }
